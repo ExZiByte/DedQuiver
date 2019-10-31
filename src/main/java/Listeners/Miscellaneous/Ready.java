afa79b42a2c83d847d075bccd72597cdd48da7be
ExZiByte <exzibyte@gmail.com>
2019-10-31 12:10:45 -0500

    Starting recode of Quiver without JavaFX

---
+++ src/main/java/Listeners/Miscellaneous/Ready.java
@@ -0,0 +1,9 @@
+package Listeners.Miscellaneous;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Ready extends ListenerAdapter {
+
+    
+
+}
---
+++ src/main/java/Quiver.java
@@ -0,0 +1,30 @@
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.api.sharding.DefaultShardManager;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+import java.net.UnknownHostException;
+
+public class Quiver {
+
+    private static ShardManager manager;
+    private static final DefaultShardManagerBuilder quiver = new DefaultShardManagerBuilder();
+
+
+    public static void main(String[] args) throws RateLimitedException, InterruptedException, UnknownHostException, LoginException {
+        quiver.setToken("");
+        quiver.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        quiver.setActivity(Activity.watching("the loading bar fill"));
+        quiver.setShardsTotal(-1);
+
+        quiver.addEventListeners(
+                Ready.
+        );
+
+        manager = quiver.build();
+    }
+
+}
