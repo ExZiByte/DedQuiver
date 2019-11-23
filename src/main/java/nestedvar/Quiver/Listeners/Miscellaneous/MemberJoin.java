363cc79af8af9761c555964f939212383a3d015c
ExZiByte <exzibyte@gmail.com>
2019-11-22 22:38:48 -0600

    Started MemberJoin listener to add the member to the database if they aren't already there

--- src/main/java/nestedvar/Quiver/Listeners/Miscellaneous/MemberJoin.java
+++ src/main/java/nestedvar/Quiver/Listeners/Miscellaneous/MemberJoin.java
@@ -1,4 +1,12 @@
 package nestedvar.Quiver.Listeners.Miscellaneous;
 
-public class MemberJoin {
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MemberJoin extends ListenerAdapter {
+
+    public void onGuildMemberJoin(GuildMemberJoinEvent event){
+
+    }
+
 }
