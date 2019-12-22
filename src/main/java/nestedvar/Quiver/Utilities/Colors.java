80bf3bc8db95a7c652be4db7a0616602eb66d039
ExZiByte <exzibyte@gmail.com>
2019-12-21 18:46:34 -0600

    Added errorRed fatalRed\ and "successGreen" color codes

--- src/main/java/nestedvar/Quiver/Utilities/Colors.java
+++ src/main/java/nestedvar/Quiver/Utilities/Colors.java
@@ -3,6 +3,7 @@ package nestedvar.Quiver.Utilities;
 public class Colors {
 
     public static int errorRed = 0xff3e30;
+    public static int fatalRed = 0x910713;
     public static int successGreen = 0x7cff73;
 
 }
